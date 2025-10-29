962e5069986084b4505d030b7d96d2e49812a478
David Jimenez <226816431+dajim2210@users.noreply.github.com>
2025-10-29 15:04:48 +0000

    Falta el Delete

---
+++ src/js/components/TodoListApi.jsx
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from "react"; 
+import { renderToStaticMarkup } from "react-dom/server";
+
+const TodoListApi = () => {
+
+    let [lista, setLista] = useState([])
+    let [tarea, setTarea] = useState("")
+
+    const API_URL = 'https://playground.4geeks.com/todo/'
+
+    const crearUsuario = () => {
+
+
+        fetch(API_URL + "users/david", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+		.then(response => response.json())  //convierte la respuesta a un formato JSON
+		.then((data) => console.log(data))  // toma los datos para mostrar en la consola
+		.catch(error => { console.log('Hubo un problema al crear el usuario: \n', error) }) //imprimir el error en la consola para depurar
+
+    }
+
+    const traerLista = () => {
+
+
+        fetch(API_URL + "users/david")
+
+            .then((response) => {
+
+                if (response.status === 404) {
+                    crearUsuario()
+                }
+
+
+                // console.log(response)
+                return response.json()
+            })
+
+            .then((data) => setLista(data.todos))
+            .catch(error => { console.log(' Oh No! Hay un problema: \n', error) })
+
+    }
+
+
+    const crearTarea = async (text) => {
+        try {
+            const response = await fetch(API_URL + "todos/alexis", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body : JSON.stringify({
+                    label: text,
+                    is_done: false
+                })
+            })
+              
+            if (!response.ok){
+                throw new Error (`Error ${response.status}: No se pudo crear la tarea.`);
+            }
+
+            const data= await response.json()
+            console.log("tarea creada con exito:", data);
+            await traerLista()
+
+
+        } catch (error) {
+                console.error('hubo un problema al crear la tarea:', error)
+        }
+
+    }
+
+
+
+    const inputText = (event) =>{
+       if(event.key === "Enter"){
+            crearTarea(tarea) // llamar a la funcion de la API con el texto de la tarea
+            setTarea("") //limpia el campo despues de enviar la tarea 
+        }
+    }
+
+
+
+
+    useEffect(() => {
+        //onload
+        // crearUsuario()
+        traerLista()
+    }, [])
+
+
+
+
+
+
+
+	return (
+		
+
+	<div className="container"> 
+				
+				<h1 className="text-center"> TodoList </h1>    
+			    <input type="text" placeholder="Que tarea desea agregar?" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={inputText}/>
+			
+        <div>
+					
+                <ul>
+                    {lista.map((item) =>(
+                        <li key={item.id}>
+                            {item.label}
+                        </li>    
+                    ))}
+
+				</ul>
+
+		</div>
+			
+			
+
+			 
+
+	</div>
+	
+
+			
+		
+	);
+};
+
+export default TodoListApi;
